3f9501daf8f604d38b8335a856e39313d1dafca4
Camilo Lima <caamilolima@gmail.com>
2021-05-04 22:38:09 -0300

    add snake movements

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ snake[0] = {
     y: 8 * box,
 }
 
+let direction = "right";
+
 let food = {
     x: Math.floor(Math.random() * 15 + 1) * box,
     y: Math.floor(Math.random() * 15 + 1) * box,
@@ -30,10 +32,69 @@ function drawFood() {
     context.fillRect(food.x, food.y, box, box);
 }
 
+document.addEventListener('keydown', update);
+
+function update(event) {
+    if(event.keyCode == 37 && direction != "right") {
+        direction = "left";
+    }
+    if(event.keyCode == 38 && direction != "down") {
+        direction = "up";
+    }
+    if(event.keyCode == 39 && direction != "left") {
+        direction = "right";
+    }
+    if(event.keyCode == 40 && direction != "up") {
+        direction = "down";
+    }
+}
+
+function defineDirection() {
+    if(snake[0].x > 15 * box && direction == "right") {
+        snake[0].x = 0;
+    }
+    if(snake[0].x < 0 * box && direction == "left") {
+        snake[0].x = 16 * box;
+    }
+    if(snake[0].y > 15 * box && direction == "down") {
+        snake[0].y = 0;
+    }
+    if(snake[0].y < 0 * box && direction == "up") {
+        snake[0].y = 16 * box;
+    }
+}
+
 function startGame() {
+    defineDirection();
+    
     createBackground();
     createSnake();
     drawFood();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") {
+        snakeX +=box;
+    }
+    if(direction == "left") {
+        snakeX -= box;
+    }
+    if(direction == "up") {
+        snakeY -= box;
+    }
+    if(direction == "down") {
+        snakeY += box;
+    }
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY,
+    }
+
+    snake.unshift(newHead);
 }
 
-startGame();
+let play = setInterval(startGame, 150);
+play;
