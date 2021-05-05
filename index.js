efb1714122f4a2a9cccfa427b65a6fd88dfe9c46
Camilo Lima <caamilolima@gmail.com>
2021-05-04 22:27:00 -0300

    create basic structure of the game

---
+++ index.js
@@ -0,0 +1,39 @@
+let canvas = document.getElementById("snake");
+let context = canvas.getContext("2d");
+let box = 32;
+
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box,
+}
+
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box,
+}
+
+function createBackground() {
+    context.fillStyle = "gray";
+    context.fillRect(0, 0, 16 * box, 16 * box);
+}
+
+function createSnake() {
+    for(let i = 0; i < snake.length; i++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
+}
+
+function startGame() {
+    createBackground();
+    createSnake();
+    drawFood();
+}
+
+startGame();
